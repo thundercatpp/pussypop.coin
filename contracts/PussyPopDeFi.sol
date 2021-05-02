e41c5447e85445b7bb7bf66bdd995685437ce5f0
git-leon <xleonhunter@gmail.com>
2021-05-02 14:31:22 -0400

    firwt commit

--- contracts/PussyPopDeFi.sol
+++ contracts/PussyPopDeFi.sol
@@ -63,7 +63,7 @@ contract PussyPopDeFi is Context, IERC20, Ownable {
     constructor () public{
         _rOwned[_msgSender()] = _rTotal;
         
-        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F);
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0xD99D1c33F9fC3444f8101754aBC46c52416550D1);
          // Create a uniswap pair for this new token
         uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
             .createPair(address(this), _uniswapV2Router.WETH());
--- truffle-config.js
+++ truffle-config.js
@@ -61,14 +61,30 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
+    testnet: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    
+    bsc: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+
     ropsten: {
-    provider: () => new HDWalletProvider({mnemonic: {phrase: process.env.MNEMONIC},
-       providerOrUrl: process.env.ROPSTEN_URL, chainId: 3}),
-    network_id: 3,       // Ropsten's id
-    gas: 8000000,        // Ropsten has a lower block limit than mainnet
-    confirmations: 1,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      provider: () => new HDWalletProvider({mnemonic: {phrase: process.env.MNEMONIC},
+        providerOrUrl: process.env.ROPSTEN_URL, chainId: 3}),
+      network_id: 3,       // Ropsten's id
+      gas: 8000000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 1,    // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
     // Useful for private networks
     // private: {
