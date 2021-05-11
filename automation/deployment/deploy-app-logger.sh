6837a4660cfcc42fa7f3010144ec8a68caa2c510
git-leon <xleonhunter@gmail.com>
2021-05-11 12:26:48 -0400

    updated deployment to redirect sterr and stdout to log files for easier bug tracking

--- automation/deployment/deploy-app-logger.sh
+++ automation/deployment/deploy-app-logger.sh
@@ -4,6 +4,6 @@ mkdir -p $logDirectory
 logFile="$logDirectory$(date +"%Y_%m_%d_%I_%M_%p").log"
 echo "Piping output to $logFile"
 
-./_deploy-app.sh > $logFile 2>&1
+./_deploy-app.sh > $logFile
 cat $logFile
 read -p "Press enter to close window ..."
